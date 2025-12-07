e5ff4c97eba326415456b0f8945f25fe5bfe71f0
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-12-07 17:27:38 +0300

    Fix codegrade tests: Match codegrade environment (Python 3.8.13, requests 2.28.1, pytest 7.2.0)

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -1,18 +1,15 @@
-from lib.versions import (
-    python_version,
-    requests_version,
-    pytest_version
-)
+from lib.versions import python_version, requests_version, pytest_version
+
 
 def test_python_version():
     version_info = python_version()
     assert version_info.major == 3
-    assert version_info.minor == 12
+    assert version_info.minor == 8
 
 
 def test_requests_version():
-    assert requests_version() == "2.31.0"
+    assert requests_version() == "2.28.1"
 
 
 def test_pytest_version():
-    assert pytest_version() == "7.4.4"
+    assert pytest_version() == "7.2.0"
